45356206556f3f54b7e57473181791bc5ba1970b
vendingtechnik <pasquale.pari@vendingtechnik.com>
2015-01-30 00:54:23 +0100

    Fehlerkorrektur

--- oxid_spezialgruppe/SQL_Skripte/2_vtec_new_oxv_view.sql
+++ oxid_spezialgruppe/SQL_Skripte/2_vtec_new_oxv_view.sql
@@ -1,4 +1,4 @@
-DROP VIEW IF EXISTS `oxv_oxarticles_de_vsg0`;
+DROP VIEW IF EXISTS `oxv_oxarticles_de_vsg`;
 
 CREATE VIEW `oxv_oxarticles_de_vsg` AS SELECT * FROM `oxv_oxarticles_de` WHERE (`oxv_oxarticles_de`.`vtecspezialgruppe` = 0);
 
